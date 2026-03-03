795f87106460bdc83f967508aa7624877317abb1
ryanouh7-cpu <ryanouh7@gmail.com>
2026-03-03 23:04:59 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  botName: "Uun",
+
+  colors: {
+    main: 0x2b2d31,
+    success: 0x00ff99,
+    warning: 0xff9900,
+    danger: 0xff0000
+  },
+
+  autoCloseHours: 10,
+  warningBeforeCloseHours: 9
+};
